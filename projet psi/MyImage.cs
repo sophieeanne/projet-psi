5da7644ae7653caf37e3174b7574b931fac17645
sophieeanne <annesophieassiene@gmail.com>
2024-03-08 16:00:03 +0100

    classe myimage

---
+++ projet psi/MyImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Media;
+using System.ComponentModel;
+
+namespace projet_psi
+{
+    internal class MyImage
+    {
+        string myfile;
+
+        public MyImage(string file)
+        {
+            myfile = file;
+
+        }
+    }
+}
