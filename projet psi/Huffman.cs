e8d88b4363f0bda08fb3cc4538a85fd2186770ed
sophieeanne <annesophieassiene@gmail.com>
2024-05-02 23:56:51 +0200

    algo d'huffman

--- projet psi/Huffman.cs
+++ projet psi/Huffman.cs
@@ -8,8 +8,31 @@ namespace projet_psi
 {
     internal class Huffman
     {
-        public Noeud root;
-        public Dictionary<Pixel, string> fréquences;
+        public Noeud root { get; set;}
+        public Dictionary<Pixel, int> fréquences = new Dictionary<Pixel, int>();
+        public List<Noeud> feuilles = new List<Noeud>();
+
+        public void Algo_Huffman()
+        {
+            foreach (KeyValuePair<Pixel, int> symbole in fréquences)
+            {
+                feuilles.Add(new Noeud(symbole.Key, symbole.Value));
+            }
+            while (feuilles.Count > 1)
+            {
+                List<Noeud> feuillestriees = feuilles.OrderBy(noeud => noeud.frequence).ToList<Noeud>();
+                if (feuillestriees.Count >= 2)
+                {
+                    List<Noeud> deuxpremiers = feuillestriees.Take(2).ToList<Noeud>();
+                    Noeud parent = new Noeud(deuxpremiers[0], deuxpremiers[1]);
+                    feuilles.Remove(deuxpremiers[0]);
+                    feuilles.Remove(deuxpremiers[1]);
+                    feuilles.Add(parent);
+                }
+                this.root = feuilles.FirstOrDefault();
+            }
+
+        }
 
 
 
