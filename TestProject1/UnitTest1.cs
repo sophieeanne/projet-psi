23ffab55692af9ae7f30eeab3e3e379d23a433bd
Gabriel GHANEM <gabriel.ghanem@edu.devinci.fr>
2024-05-03 10:13:22 +0200

    4e test decodeImage1

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -19,6 +19,22 @@ namespace TestProject1
             Assert.That(File.Exists(filePath), "Le fichier doit exister après l'exécution de From_Image_To_File.");
             File.Delete(filePath); // Cleanup
         }
+        [Test]
+
+        public void TestDecoderImage1()
+        {
+            // Arrange
+            var image = new MyImage(new Pixel[,] { { new Pixel(255, 255, 255) } }, 1, 1);
+            string filePath = "test.bmp";
+            image.From_Image_To_File(filePath);
+            var image2 = new MyImage(new Pixel[,] { { new Pixel(255, 255, 255) } }, 1, 1);
+            // Act
+            image2.Decoder_Image1(image);
+
+            // Assert
+            Assert.That(image2, Is.EqualTo(image), "Les deux images doivent être égales.");
+            File.Delete(filePath); // Cleanup
+        }   
 
     }
 }
