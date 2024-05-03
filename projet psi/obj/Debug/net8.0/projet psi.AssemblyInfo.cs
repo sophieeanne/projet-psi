1c798db5f5d342c1be80d39a649df7f535dbc3fc
Gabrielgorithme <gabriel.ghanem@gmail.com>
2024-05-03 13:46:50 +0200

    ..

--- projet psi/obj/Debug/net8.0/projet psi.AssemblyInfo.cs
+++ projet psi/obj/Debug/net8.0/projet psi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projet psi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61ddc119041d4dac9d4fd8951fa506e49db3a3e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("projet psi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projet psi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
